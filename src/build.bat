0a279118277115eec5d3763f95de67ead05cce55
Kgopolo Ibang <wrld999.010@gmail.com>
2025-12-14 20:56:31 +0200

    Update build.bat

--- src/build.bat
+++ src/build.bat
@@ -1,7 +1,7 @@
 @echo off
 if not exist ..\GameAPP mkdir ..\GameAPP
 g++ -std=c++20 -fmodules-ts -c GenericsProto.cpp
-g++ -std=c++20 -fmodules-ts -c Tic-Tac-Toe.cpp
+g++ -std=c++20 -fmodules-ts -c TicTacToe.cpp
 g++ -std=c++20 -fmodules-ts -c main.cpp
 
 g++ -std=c++20 -fmodules-ts -o ..\GameAPP\App GenericsProto.o Tic-Tac-Toe.o main.o 
